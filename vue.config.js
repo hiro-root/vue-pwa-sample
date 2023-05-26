1712c34af048ed669ed1e232219d10dd4c083636
hiro-root <hiro@rooooooot.com>
2023-05-26 16:58:52 +0900

    Add docs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './',
+  assetsDir: './',
+  // baseUrl: process.env.NODE_ENV === 'production' ? '/vue-pwa-sample' : '/',
+  outputDir: 'docs'
 })
